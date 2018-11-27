50b2fa255ad4aa8b025bb4670457cf06a16953de
s7thiago <thyagosousasilva@gmail.com>
2018-11-27 15:21:40 -0200

    Refetornado o projeto, e trocando para modelo compatível com netbeans

--- Projeto/main.c
+++ Projeto/main.c
@@ -1,8 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: alunotgn
+ *
+ * Created on 27 de Novembro de 2018, 15:20
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
 }
+
