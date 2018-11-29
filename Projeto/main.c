0217f4cf4ac81bfef1a0b33c5cd2d11c22689584
s7thiago <thyagosousasilva@gmail.com>
2018-11-29 00:40:03 -0200

    Atualizações não relevantes na estrutura gerada autometicamente do projeto

--- Projeto/main.c
+++ Projeto/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "sistema_academico.h"
 
-int main()
-{
-    printf("Hello world!\n");
+int main() {
+
+    FILE *arquivo = fopen("meuArquivo.txt", "w");
+
+    char nome[] = "";
+    printf("Digite o nome do aluno: ");
+    scanf("%s", &nome);
+
+    printf("nome recebido: %s", nome);
+
+    fprintf(nome, "%s", arquivo);
+    fclose(arquivo);
+
     return 0;
 }
