0d23a86640f3974ba7c084d8376a576b3a3ba6ee
Thiago Sousa <thyagosousasilva@gmail.com>
2018-11-29 16:33:24 -0200

    Iniciada a construção da função que salva os dados de um aluno no arquivo em disco

--- Projeto/Funcoes.c
+++ Projeto/Funcoes.c
@@ -3,8 +3,10 @@
 #include <string.h>
 #include "funcoes.h"
 
+#define savesPath "saves\\"
+
 //Pasta padrao
-char defaultPath[] = "saves\\";
+char defaultPath[] = savesPath;
 
 //Definindo tipo string
 typedef char string;
@@ -13,7 +15,38 @@ void salvarAluno(aluno a){
     printf("\n\nSalvar aluno foi acionada\n\n");
 
     //concatenando o caminho para salvar o arquivo
+    strcat(a.nome, ".txt");
 
-    FILE *arquivo = fopen(a.matricula, "a");
+    FILE *arquivo = fopen("arquivo", "a");
+    fputs(retornaPrimeiroNome(a.nome), arquivo);
+    fclose(arquivo);
+}
+
+char * retornaPrimeiroNome(char *nome) {
+
+    fflush(stdin);
+    //conta quantas letras tem o primeiro nome
+    int letrasPos = 0;
+    while(nome[letrasPos] != ' ') {
+        letrasPos++;
+    }
+
+    //printf("\nTamanho do primeiro nome: %d\n", letrasPos);
+
+    //alocando memoria necessaria para o nome recebido
+    char *retorno = malloc(letrasPos * sizeof(char));
+
+    printf("\nTamanho do vetRetorno: %d\n\n", (letrasPos*sizeof(char)));
+
+    //Copia o primeiro a parte correspondento ao primeiro nome para o vetor retorno:
+    for(int i = 0; i < letrasPos; i++) {
+        printf("copiando '%c'\n", nome[i]);
+        retorno[i] = nome[i];
+    }
+
+    printf("Primeiro nome antes de retornar: %s", retorno);
+
+    return retorno;
+    free(retorno);
 }
 
--- Projeto/main.c
+++ Projeto/main.c
@@ -7,13 +7,18 @@ int main() {
     aluno a;
     a.matricula = 826484;
     a.media = 7;
-    a.nome = "Mariana Silva";
+    a.nome = "Fernanda Josefa";
     a.nota1 = 7;
     a.nota2 = 7;
 
     salvarAluno(a);
 
     printf("Nome do aluno: %s\n\n", a.nome);
+
+    //
+    char *primeiroNome = retornaPrimeiroNome(a.nome);
+    printf("\n\nprimeiro nome: ");
+    puts(primeiroNome);
     return 0;
 }
 
